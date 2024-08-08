1e4fbb7a24e7844abe9d1be9b5815edfcfdc5128
Ansh <anshkant090@gmail.com>
2024-08-09 00:21:05 +0530

    manage user inputs using useState

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,65 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail,setEnteredEmail] = useState('');
+  // const [enterdPassword,setEnteredPassword] = useState('');
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault(); // to prevent the browsers default submission
+    console.log("Submit button clicked");
+    console.log(enteredValues);
+  }
+
+  /*
+  instead of these separate handle change we now can use the generic handleChange
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
+  }
+  */
+
+  // to set the values changed by the user by passing the identifier i.e. (email,password)
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
